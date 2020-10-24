e115038d2c2f415a25bd50cec4ab4e0d1a7a8561
Joshua Hellard <67675776+jhellard@users.noreply.github.com>
2020-10-23 23:49:08 -0400

    Further improvements to app
    Added some login functionality, updated menubar and does not allow user to visit pages intended for logged out users.
    
    Added local storage to keep users signed in on concurrent visits.

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Menu } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
+import { AuthContext } from "../context/auth";
+
 const MenuBar = () => {
+  const { user, logout } = useContext(AuthContext);
   const pathname = window.location.pathname;
   const path = pathname === "/" ? "home" : pathname.substr(1);
 
@@ -10,7 +13,14 @@ const MenuBar = () => {
 
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
-  return (
+  const menuBar = user ? (
+    <Menu pointing secondary size="massive" color="teal">
+      <Menu.Item name={user.username} active onClick as={Link} to="/" />
+      <Menu.Menu position="right">
+        <Menu.Item name="logout" onClick={logout} />
+      </Menu.Menu>
+    </Menu>
+  ) : (
     <Menu pointing secondary size="massive" color="teal">
       <Menu.Item
         name="home"
@@ -37,6 +47,8 @@ const MenuBar = () => {
       </Menu.Menu>
     </Menu>
   );
+
+  return menuBar;
 };
 
 export default MenuBar;
