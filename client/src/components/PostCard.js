789a55fc9a0c0f69c502735dbc20bfa42a601130
Joshua Hellard <67675776+jhellard@users.noreply.github.com>
2020-10-27 22:02:12 -0400

    Added like button functionality
    The like button will now properly display if the user has liked the post previously, clicking will like the post and if the user is not logged in it will bring them to the login page.
    
    Added delete button to postst that the use has made, not yet working.

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,18 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Card, Button, Image, Icon, Label } from "semantic-ui-react";
 import moment from "moment";
 
+import { AuthContext } from "../context/auth";
+import LikeButton from "./LikeButton";
+
 const PostCard = ({
   post: { body, createdAt, id, username, likeCount, commentCount, likes },
 }) => {
-  const likePost = () => {
-    console.log("Like Post! Finish this Function!!");
-  };
-
-  const commentOnPost = () => {
-    console.log("Comment on the post! Finish this function!!");
-  };
+  const { user } = useContext(AuthContext);
 
   return (
     <Card fluid>
@@ -29,23 +26,25 @@ const PostCard = ({
         <Card.Description>{body}</Card.Description>
       </Card.Content>
       <Card.Content extra>
-        <Button as="div" labelPosition="right">
-          <Button color="teal" basic onClick={likePost}>
-            <Icon name="heart" />
-          </Button>
-          <Label basic color="teal" pointing="left">
-            {likeCount}
-          </Label>
-        </Button>
-
-        <Button as="div" labelPosition="right">
-          <Button color="blue" basic onClick={commentOnPost}>
+        <LikeButton user={user} post={{ id, likes, likeCount }} />
+        <Button labelPosition="right" as={Link} to={`/posts/${id}`}>
+          <Button color="blue" basic>
             <Icon name="comments" />
           </Button>
           <Label basic color="blue" pointing="left">
             {commentCount}
           </Label>
         </Button>
+        {user && user.username === username && (
+          <Button
+            as="div"
+            color="red"
+            floated="right"
+            onClick={() => console.log("Delete Post")}
+          >
+            <Icon name="trash" style={{ margin: 0 }} />
+          </Button>
+        )}
       </Card.Content>
     </Card>
   );
